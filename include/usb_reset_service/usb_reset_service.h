4fc30196e3111d6956a7d1469279342226221410
Markus Herb <mail@markusherb.de>
2016-01-15 22:00:36 +0100

    Add service implementation

--- include/usb_reset_service/usb_reset_service.h
+++ include/usb_reset_service/usb_reset_service.h
@@ -12,6 +12,14 @@ class UsbResetService : public lms::Service {
 public:
     bool init() override;
     void destroy() override;
+
+    /**
+     * @brief Reset USB device by VID/PID
+     * @param vendorId The USB Vendor ID of the device
+     * @param productId The USB Product ID of the device
+     * @return True on success, false on failure
+     */
+    bool reset(uint16_t vendorId, uint16_t productId);
 };
 
 } // namespace usb_reset_service
--- src/usb_reset_service.cpp
+++ src/usb_reset_service.cpp
@@ -1,5 +1,8 @@
 #include "usb_reset_service/usb_reset_service.h"
 
+#include <cstdlib>
+#include <iomanip>
+
 namespace usb_reset_service {
 
 bool UsbResetService::init() {
@@ -9,4 +12,28 @@ bool UsbResetService::init() {
 void UsbResetService::destroy() {
 }
 
+bool UsbResetService::reset(uint16_t vendorId, uint16_t productId) {
+
+    auto path = config().get<std::string>("path", "/usr/bin/usb-reset");
+    if(path.empty()) {
+        logger.error("path") << "Reset script path is not set or empty";
+        return false;
+    }
+
+    std::stringstream command;
+    command << path << " "
+            << std::hex << std::showbase << std::setw(4) << std::setfill('0')
+            << vendorId << " " << productId;
+
+    logger.debug("command") << command.str();
+
+    auto ret = std::system(command.str().c_str());
+
+    if( EXIT_SUCCESS == ret ) {
+        return true;
+    } else {
+        logger.error("reset") << "Unable to reset device (Exit code " << ret << ")";
+        return false;
+    }
+}
 } // namespace usb_reset_service
